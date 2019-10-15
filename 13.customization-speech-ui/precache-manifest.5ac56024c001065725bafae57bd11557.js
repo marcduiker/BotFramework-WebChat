85c0ff3027853cec7afc16736576b1ad15190668
XxxxxYyyyy <v-bruhal@microsoft.com>
2019-10-15 10:15:46 +0000

    Automated push from build 83459

---
+++ 13.customization-speech-ui/precache-manifest.5ac56024c001065725bafae57bd11557.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87a48526ee82f166d945be6255566d9",
+    "url": "/BotFramework-WebChat/13.customization-speech-ui/index.html"
+  },
+  {
+    "revision": "6a8fdc2244363844870a",
+    "url": "/BotFramework-WebChat/13.customization-speech-ui/static/css/main.86d1088d.chunk.css"
+  },
+  {
+    "revision": "65f84bfe5aa86de131dd",
+    "url": "/BotFramework-WebChat/13.customization-speech-ui/static/js/2.00fe5b8a.chunk.js"
+  },
+  {
+    "revision": "6a8fdc2244363844870a",
+    "url": "/BotFramework-WebChat/13.customization-speech-ui/static/js/main.57b33aa3.chunk.js"
+  },
+  {
+    "revision": "127474629dceca3dcf83",
+    "url": "/BotFramework-WebChat/13.customization-speech-ui/static/js/runtime~main.100942fb.js"
+  }
+]);
